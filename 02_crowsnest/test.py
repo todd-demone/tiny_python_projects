6c4e2c34b06217a80c03df17d26f0a2b870818a9
Todd Demone <tdemone1@gmail.com>
2025-11-11 12:05:13 -0500

    finish chapters 1 and 2

--- 02_crowsnest/test.py
+++ 02_crowsnest/test.py
@@ -4,15 +4,34 @@
 import os
 from subprocess import getstatusoutput, getoutput
 
-prg = './crowsnest.py'
+prg = "./crowsnest.py"
 consonant_words = [
-    'brigantine', 'clipper', 'dreadnought', 'frigate', 'galleon', 'haddock',
-    'junk', 'ketch', 'longboat', 'mullet', 'narwhal', 'porpoise', 'quay',
-    'regatta', 'submarine', 'tanker', 'vessel', 'whale', 'xebec', 'yatch',
-    'zebrafish'
+    "brigantine",
+    "clipper",
+    "dreadnought",
+    "frigate",
+    "galleon",
+    "haddock",
+    "junk",
+    "ketch",
+    "longboat",
+    "mullet",
+    "narwhal",
+    "porpoise",
+    "quay",
+    "regatta",
+    "submarine",
+    "tanker",
+    "vessel",
+    "whale",
+    "xebec",
+    "yatch",
+    "zebrafish",
 ]
-vowel_words = ['aviso', 'eel', 'iceberg', 'octopus', 'upbound']
-template = 'Ahoy, Captain, {} {} off the larboard bow!'
+vowel_words = ["aviso", "eel", "iceberg", "octopus", "upbound"]
+template = "Ahoy, Captain, {} {} off the larboard bow!"
+
+nonalpha_words = ["123kid", "!peligro!"]
 
 
 # --------------------------------------------------
@@ -26,10 +45,10 @@ def test_exists():
 def test_usage():
     """usage"""
 
-    for flag in ['-h', '--help']:
-        rv, out = getstatusoutput(f'{prg} {flag}')
+    for flag in ["-h", "--help"]:
+        rv, out = getstatusoutput(f"{prg} {flag}")
         assert rv == 0
-        assert out.lower().startswith('usage')
+        assert out.lower().startswith("usage")
 
 
 # --------------------------------------------------
@@ -37,8 +56,8 @@ def test_consonant():
     """brigantine -> a brigantine"""
 
     for word in consonant_words:
-        out = getoutput(f'{prg} {word}')
-        assert out.strip() == template.format('a', word)
+        out = getoutput(f"{prg} {word}")
+        assert out.strip() == template.format("a", word)
 
 
 # --------------------------------------------------
@@ -46,8 +65,8 @@ def test_consonant_upper():
     """brigantine -> a Brigatine"""
 
     for word in consonant_words:
-        out = getoutput(f'{prg} {word.title()}')
-        assert out.strip() == template.format('a', word.title())
+        out = getoutput(f"{prg} {word.title()}")
+        assert out.strip() == template.format("a", word.title())
 
 
 # --------------------------------------------------
@@ -55,8 +74,8 @@ def test_vowel():
     """octopus -> an octopus"""
 
     for word in vowel_words:
-        out = getoutput(f'{prg} {word}')
-        assert out.strip() == template.format('an', word)
+        out = getoutput(f"{prg} {word}")
+        assert out.strip() == template.format("an", word)
 
 
 # --------------------------------------------------
@@ -64,5 +83,44 @@ def test_vowel_upper():
     """octopus -> an Octopus"""
 
     for word in vowel_words:
-        out = getoutput(f'{prg} {word.upper()}')
-        assert out.strip() == template.format('an', word.upper())
+        out = getoutput(f"{prg} {word.upper()}")
+        assert out.strip() == template.format("an", word.upper())
+
+
+# fmt: off
+# def test_vowel_upper_match_case():
+    # """Octopus -> An Octopus"""
+#
+    # for word in vowel_words:
+        # out = getoutput(f"{prg} {word.upper()}")
+        # assert out.strip() == template.format("An", word.upper())
+# fmt: on
+
+
+# fmt: off
+# def test_consonant_upper_match_case():
+    # """Brigantine -> A Brigantine"""
+#
+    # for word in consonant_words:
+        # out = getoutput(f"{prg} {word.upper()}")
+        # assert out.strip() == template.format("A", word.upper())
+# fmt: on
+
+
+# fmt: off
+# def test_starboard():
+    # """--side starboard"""
+# 
+    # for word in consonant_words:
+        # out = getoutput(f"{prg} {word} --side starboard")
+        # assert out.strip() == "Ahoy, Captain, {} {} off the {} bow!".format(
+            # "a", word, "starboard"
+        # )
+# fmt: on
+
+
+def test_non_alpha():
+    """123kid -> 'Try again."""
+    for word in nonalpha_words:
+        out = getoutput(f"{prg} {word}")
+    assert out == "Try again."
