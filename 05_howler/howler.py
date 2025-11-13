a08f2a3ce9fd94d9baf8941c327fac154f93ba15
Todd Demone <tdemone1@gmail.com>
2025-11-13 13:50:58 -0500

    finish chapter 5

---
+++ 05_howler/howler.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+"""
+Author : Todd Demone <tdemone1@gmail.com>
+Date   : 2025-11-13
+Purpose: Howler (upper-cases input)
+"""
+
+import argparse
+import os
+import sys
+
+
+# --------------------------------------------------
+def get_args():
+    """Get command-line arguments"""
+
+    parser = argparse.ArgumentParser(
+        description="Howler (upper-cases input)",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+
+    parser.add_argument("text", metavar="text", type=str, help="Input string or file")
+
+    parser.add_argument(
+        "-o",
+        "--outfile",
+        help="Output filename",
+        metavar="str",
+        default="",
+    )
+
+    args = parser.parse_args()
+
+    if os.path.isfile(args.text):
+        args.text = open(args.text).read().rstrip()
+
+    return args
+
+
+# --------------------------------------------------
+def main():
+    """The main event"""
+
+    args = get_args()
+    out_fh = open(args.outfile, "wt") if args.outfile else sys.stdout
+    out_fh.write(args.text.upper() + "\n")
+    out_fh.close()
+
+
+# --------------------------------------------------
+if __name__ == "__main__":
+    main()
