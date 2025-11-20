38cf5cea575fc964645817ee92451f47f484dd50
Todd Demone <tdemone1@gmail.com>
2025-11-19 21:30:00 -0500

    finish ch 13

--- 13_twelve_days/twelve_days.py
+++ 13_twelve_days/twelve_days.py
@@ -2,10 +2,11 @@
 """
 Author : Todd Demone <tdemone1@gmail.com>
 Date   : 2025-11-18
-Purpose: Rock the Casbah
+Purpose: Twelve Days of Christmas
 """
 
 import argparse
+import sys
 
 
 # --------------------------------------------------
@@ -13,40 +14,33 @@ def get_args():
     """Get command-line arguments"""
 
     parser = argparse.ArgumentParser(
-        description='Rock the Casbah',
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+        description="Twelve Days of Christmas",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
 
-    parser.add_argument('positional',
-                        metavar='str',
-                        help='A positional argument')
+    parser.add_argument(
+        "-n",
+        "--num",
+        help="Number of days to sing",
+        metavar="days",
+        type=int,
+        default=12,
+    )
 
-    parser.add_argument('-a',
-                        '--arg',
-                        help='A named string argument',
-                        metavar='str',
-                        type=str,
-                        default='')
+    parser.add_argument(
+        "-o",
+        "--outfile",
+        help="Outfile",
+        metavar="FILE",
+        type=argparse.FileType("wt"),
+        default=sys.stdout,
+    )
 
-    parser.add_argument('-i',
-                        '--int',
-                        help='A named integer argument',
-                        metavar='int',
-                        type=int,
-                        default=0)
+    args = parser.parse_args()
 
-    parser.add_argument('-f',
-                        '--file',
-                        help='A readable file',
-                        metavar='FILE',
-                        type=argparse.FileType('rt'),
-                        default=None)
-
-    parser.add_argument('-o',
-                        '--on',
-                        help='A boolean flag',
-                        action='store_true')
-
-    return parser.parse_args()
+    if args.num not in range(1, 13):
+        parser.error(f'--num "{args.num}" must be between 1 and 12')
+    return args
 
 
 # --------------------------------------------------
@@ -54,19 +48,74 @@ def main():
     """The main event"""
 
     args = get_args()
-    str_arg = args.arg
-    int_arg = args.int
-    file_arg = args.file
-    flag_arg = args.on
-    pos_arg = args.positional
+    verses = map(verse, range(1, args.num + 1))
+    print("\n\n".join(verses), file=args.outfile)
 
-    print(f'str_arg = "{str_arg}"')
-    print(f'int_arg = "{int_arg}"')
-    print('file_arg = "{}"'.format(file_arg.name if file_arg else ''))
-    print(f'flag_arg = "{flag_arg}"')
-    print(f'positional = "{pos_arg}"')
+
+def verse(day):
+    """Create a verse"""
+    ordinal = [
+        "first",
+        "second",
+        "third",
+        "fourth",
+        "fifth",
+        "sixth",
+        "seventh",
+        "eighth",
+        "ninth",
+        "tenth",
+        "eleventh",
+        "twelfth",
+    ]
+
+    gifts = [
+        "A partridge in a pear tree.",
+        "Two turtle doves,",
+        "Three French hens,",
+        "Four calling birds,",
+        "Five gold rings,",
+        "Six geese a laying,",
+        "Seven swans a swimming,",
+        "Eight maids a milking,",
+        "Nine ladies dancing,",
+        "Ten lords a leaping,",
+        "Eleven pipers piping,",
+        "Twelve drummers drumming,",
+    ]
+
+    lines = [
+        f"On the {ordinal[day - 1]} day of Christmas,",
+        "My true love gave to me,",
+    ]
+
+    lines.extend(reversed(gifts[:day]))
+
+    if day > 1:
+        lines[-1] = "And " + lines[-1].lower()
+
+    return "\n".join(lines)
+
+
+def test_verse():
+    """Test verse"""
+    assert verse(1) == "\n".join(
+        [
+            "On the first day of Christmas,",
+            "My true love gave to me,",
+            "A partridge in a pear tree.",
+        ]
+    )
+    assert verse(2) == "\n".join(
+        [
+            "On the second day of Christmas,",
+            "My true love gave to me,",
+            "Two turtle doves,",
+            "And a partridge in a pear tree.",
+        ]
+    )
 
 
 # --------------------------------------------------
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
