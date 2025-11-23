d4eeef4d1db26edf5828ae0a2c93c864fc6bbf09
Todd Demone <tdemone1@gmail.com>
2025-11-23 12:24:53 -0500

    finish ch 14

---
+++ 14_rhymer/rhymer.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+"""
+Author : Todd Demone <tdemone1@gmail.com>
+Date   : 2025-11-23
+Purpose: Make rhyming words
+"""
+
+import argparse
+import re
+import string
+
+
+# --------------------------------------------------
+def get_args():
+    """Get command-line arguments"""
+
+    parser = argparse.ArgumentParser(
+        description='Make rhyming "words"',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+
+    parser.add_argument("word", metavar="word", help="A word to rhyme")
+
+    return parser.parse_args()
+
+
+# --------------------------------------------------
+def main():
+    """The main event"""
+
+    args = get_args()
+    prefixes = (
+        list("bcdfghjklmnpqrstvwxyz")
+        + (
+            "bl br ch cl cr dr fl fr gl gr pl pr sc "
+            "sh sk sl sm sn sp st sw th tr tw thw wh wr "
+            "sch scr shr sph spl spr squ str thr"
+        ).split()
+    )
+
+    start, rest = stemmer(args.word)
+    if rest:
+        print("\n".join(sorted([p + rest for p in prefixes if p != start])))
+    else:
+        print(f'Cannot rhyme "{args.word}"')
+
+
+def stemmer(word):
+    """Return leading consonants (if any), and 'stem' of word"""
+    word = word.lower()
+    vowels = "aeiou"
+    consonants = "".join([c for c in string.ascii_lowercase if c not in vowels])
+    pattern = "([" + consonants + "]+)?" "([" + vowels + "])" "(.*)"
+
+    match = re.match(pattern, word)
+    if match:
+        p1 = match.group(1) or ""
+        p2 = match.group(2) or ""
+        p3 = match.group(3) or ""
+        return (p1, p2 + p3)
+    else:
+        return (word, "")
+
+
+def test_stemmer():
+    """Test stemmer"""
+    assert stemmer("") == ("", "")
+    assert stemmer("cake") == ("c", "ake")
+    assert stemmer("chair") == ("ch", "air")
+    assert stemmer("APPLE") == ("", "apple")
+    assert stemmer("RDNZL") == ("rdnzl", "")
+    assert stemmer("123") == ("123", "")
+
+
+# --------------------------------------------------
+if __name__ == "__main__":
+    main()
